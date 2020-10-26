6f7861056552ebad7411c03d686b3cdb321728e9
nelo <nelokwaz@gmail.com>
2020-10-25 15:01:10 -1200

    installed material ui library

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <h1>Let's build a covid 19 tracker</h1>
+      {/** HEADER */}
+      {/**Title + Dropdown */}
+      {/* INFO BOXES*/}
+      {/**Info Box 1 */}
+      {/**Info Box 2 */}
+      {/**Info Box 3s */}
+      {/** MAP */}
+      {/* TABLE*/}
+      {/** GRAPH */}
     </div>
   );
 }
