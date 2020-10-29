990f9c3b1eff3df7b5bd41bd82930e065e8398e2
nelo <nelokwaz@gmail.com>
2020-10-28 19:09:10 -1200

    pulled out all country info data on component mount

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
   const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => setCountryInfo(data));
+  }, []);
+
+  useEffect(() => {
     const getCountriesData = async () => {
       const countriesFetched = await fetch(
         "https://disease.sh/v3/covid-19/countries"
