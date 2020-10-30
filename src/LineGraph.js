02c7fbd34a80cc193f8fa712ef49afd6b79c9f0b
nelo <nelokwaz@gmail.com>
2020-10-29 18:54:39 -1200

    installed numberal number formatter package

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
+import { buildChartData } from "./utils";
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+};
 
 const LineGraph = () => {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        const chartData = buildChartData(data);
+        setData(chartData);
+      });
   }, []);
+
   return (
     <div>
       <h1>I am a line graph</h1>
-      <Line />
+      <Line
+        options={options}
+        data={{
+          datasets: [
+            {
+              data: data,
+            },
+          ],
+        }}
+      />
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,21 @@ export const sortData = (data) => {
   const sortedData = [...data];
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
+
+export const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+  console.log(typeof data[casesType]);
+  for (let date of data[casesType]) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+
+  return chartData;
+};
