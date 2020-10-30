273fbb5fdf83b8b782eeda43d163b3b7fe133ac2
nelo <nelokwaz@gmail.com>
2020-10-29 18:13:42 -1200

    installed chart.js library

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 
 const LineGraph = () => {
-  return <div></div>;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <div>
+      <h1>I am a line graph</h1>
+      <Line />
+    </div>
+  );
 };
 
 export default LineGraph;
