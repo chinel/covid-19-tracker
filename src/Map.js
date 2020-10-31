03870d2acf74f3cd801c1176fbfda72dcbe07a37
nelo <nelokwaz@gmail.com>
2020-10-30 17:53:27 -1200

    added some styling to the map

--- src/Map.js
+++ src/Map.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
 
-const Map = () => {
+const Map = ({ zoom, center }) => {
   return (
     <div className="map">
-      <LeafletMap>
+      <LeafletMap zoom={zoom} center={center}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
